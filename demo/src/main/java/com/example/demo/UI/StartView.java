bbadb52b37985c41e973ea0c0c7b7a8b83fd3c8e
Łukasz Mencel <59538107+vvooki@users.noreply.github.com>
2022-06-12 23:26:07 +0200

    Logging View

---
+++ demo/src/main/java/com/example/demo/UI/StartView.java
@@ -0,0 +1,32 @@
+package com.example.demo.UI;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+
+@Route("")
+public class StartView extends VerticalLayout {
+
+    private Button button = new Button("ADMIN", e -> admin());
+    private Button button1 = new Button("KLIENT", buttonClickEvent -> client());
+
+
+    @PostConstruct
+    private void init() {
+        add(button, button1);
+    }
+
+    private void admin() {
+        UI.getCurrent().getPage().executeJs("window.location.href='/DashboardAdmin'");
+    }
+
+    private void client() {
+        UI.getCurrent().getPage().executeJs("window.location.href='/DashboardClient'");
+    }
+}
